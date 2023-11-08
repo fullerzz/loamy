5e8a3fca23768d251ca556a36c596b0dd4f7d217
Zach Fuller <zach.fuller1222@gmail.com>
2023-11-07 19:39:06 -0700

    initial test setup

---
+++ tests/test_model_validation.py
@@ -0,0 +1,17 @@
+from zconcurrent.zsession import RequestMap
+from typing import Any, Dict
+
+
+def test_valid_RequestMap(base_config: Dict[str, Any]):
+    requestMap = RequestMap(
+        url=base_config["url"],
+        httpOperation="GET",
+        body=base_config["body"],
+        queryParams=base_config["queryParams"],
+        headers=base_config["headers"],
+    )
+    assert requestMap.url == base_config["url"]
+    assert requestMap.httpOperation == "GET"
+    assert requestMap.body == base_config["body"]
+    assert requestMap.queryParams == base_config["queryParams"]
+    assert requestMap.headers == base_config["headers"]
